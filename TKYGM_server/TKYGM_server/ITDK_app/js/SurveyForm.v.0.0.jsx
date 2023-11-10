96a8142e9d1a401a7dd669de9899e4bca2bbbc23
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-11-10 18:58:46 +0300

    feature - articles are added to SurveyForm up to the 27th.

--- TKYGM_server/TKYGM_server/ITDK_app/js/SurveyForm.v.0.0.jsx
+++ TKYGM_server/TKYGM_server/ITDK_app/js/SurveyForm.v.0.0.jsx
@@ -136,6 +136,192 @@ function SurveyForm() {
                             { id: "q-7-4", head: "Yapı kayıt belgesi ve belgeye başvuru aşamasında ilgili sisteme yüklenen yapı fotoğrafı sunulmuş mu? Belge ve fotoğraf ile fiili durum uyumlu mu?" },
                         ]} />
 
+                <QuestionBody
+                    id="8"
+                    head="Onaylı tesis organizasyonu ve yerleşim planı mevcut mu?"
+                    items={
+                        [
+                            { id: "q-8-1", head: "Ek 8 (Onaylı planın ölçekli taratılmış kopyası sunulmuş mu?)" },
+                            { id: "q-8-2", head: "Onaylı tesis organizasyonu ve yerleşim planı ile tesisin fiili durumu uyumlu mu?" },
+                            { id: "q-8-3", head: "Onaylanmak üzere sunulmuş taslak Plan mevcut mu?" },
+                        ]} />
+                <QuestionBody
+                    id="9"
+                    head="Dolgu ve kıyı yapılarının, uygulama projelerine uygun olarak tamamlandığına dair Altyapı Yatırımları Genel Müdürlüğünün yazısı mevcut mu? (Yönetmelik Md -11 1 (e) ) - Kısmi İşletme İzni için aranmaz."
+                    items={
+                        [
+                            { id: "q-9-1", head: "Ek 9 " },
+                            { id: "q-9-2", head: "Dolgu alanlarının değerlendirildiği onaylı Proje/Uygunluk yazısı mevcut mu?" },
+                        ]} />
+                <QuestionBody
+                    id="10"
+                    head="Kapasite raporu mevcut mu? (Yönetmelik Md -11 1 (ğ) )"
+                    items={
+                        [
+                            { id: "q-10-1", head: "Ek 10" },
+                            { id: "q-10-2", head: "Belge geçerlilik tarihi denetim tarihinden sonra mı?" },
+                            { id: "q-10-3", head: "Firma unvanı ve adres bilgisi Ticaret Sicil Gazetesi ile uyumlu mu?" },
+                            { id: "q-10-3", head: "Faaliyet alanı bilgisi fiili durumla uyumlu mu?" },
+                            { id: "q-10-3", head: "Arazi (m2), iskele, kızak, vinç, mühendis sayısı (dönemsel değişkenlik gösterebileceği dikkate alınır) fiili durumla uyumlu mu?" },
+                        ]} />
+                <QuestionBody
+                    id="11"
+                    head="İlgili kurumundan alınmış tesisin yangınla mücadelede yeterli olduğuna dair belge mevcut mu? (Yönetmelik Md -11 1 (g) )"
+                    items={
+                        [
+                            { id: "q-11-1", head: "Ek 11" },
+                            { id: "q-11-2", head: "Doküman içeriğinde işlenmiş bilgiler ile fiili durum uyumlu mu?" },
+                        ]} />
+                <QuestionBody
+                    id="12"
+                    head="Tesisler için TS EN ISO 9001 Kalite Yönetim Sistemi Belgesi, TS EN ISO 14001 Çevre Yönetim Sistemi Belgesi ve OHSAS 18001 İş Sağlığı ve Güvenliği Yönetim Sistemi Belgesinin aslı veya idarece onaylı örneği mevcut mu? (Yönetmelik Md -11 1 (h) ) - Kısmi İşletme İzni için aranmaz."
+                    items={
+                        [
+                            { id: "q-12-1", head: "Ek 12" },
+                        ]} />
+                <QuestionBody
+                    id="13"
+                    head="İnşa, tadilat ve bakım-onarım için tesiste yeterli donanım bulunup bulunmadığı (Yönetmelik Md 12 (1)"
+                    items={
+                        [
+                            { id: "q-13-1", head: "Ek 13" },
+                            { id: "q-13-2", head: "İş akış şemasıyla uyumlu yapı/donatı ile techizat mevcut mu?" },
+                            { id: "q-13-3", head: "Tesisin mevcut durumu ve işleyiş ile uyumlu organizasyon ve iş akış şeması mevcut mu?" },
+                            { id: "q-13-3", head: "Tesis organizasyonunda modern üretim ve yönetim meotdları dikkate alınmış mı?" },
+                            { id: "q-13-3", head: "Kapalı mahallerde havalandırma yeterli mi?" },
+                            { id: "q-13-3", head: "Tesisin genelinde aydınlatma yeterli mi?" },
+                            { id: "q-13-3", head: "İlgili şemalar (Organizasyon ve İş Akışı vb.) ile firma yetkilisinin üretim ve sahaların kullanımına yönelik izahlarının uyumlu olduğu gözlendi mi?" },
+                            { id: "q-13-3", head: "İlgili şemaların fiili durumla uyumlu olduğu gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="14"
+                    head="Tersanelerde en az iki gemi mühendisi olmak kaydıyla daimi ve altyüklenici olarak tüm çalışanların %2’si mühendis, bunun da yarısı gemi mühendisi mi? 
+Tekne imal yeri ve imalat yapan çekek yerlerinde en az bir gemi mühendisi daimi olarak bulundurulmakta mı veya bir tescilli serbest gemi mühendisliği bürosu ile anlaşma 
+yapıldığına dair sözleşme mevcut mu? (Yönetmelik Md 17 (7) a) b)"
+                    items={
+                        [
+                            { id: "q-14-1", head: "Ek 14" },
+                            { id: "q-14-2", head: "Oda sicil no bilgisi alındı mı? Koşullar sağlanıyor mu?" },
+                        ]} />
+                <QuestionBody
+                    id="15"
+                    head="Çevresel Etki Değerlendirmesi olumlu kararı/ gerekli değildir belgesi sunuldu mu? Çevre izniyle değişecek"
+                    items={
+                        [
+                            { id: "q-15-1", head: "Ek 15 (Karar yazısı, Çevre Lisansı ve Çevre İzni Belgelerinin nüshaları)" }, ,
+                        ]} />
+                <QuestionBody
+                    id="16"
+                    head="Yangın tahliye projesi ve ilgili planlar mevcut mu?"
+                    items={
+                        [
+                            { id: "q-16-1", head: "Ek 16" },
+                            { id: "q-16-2", head: "Yüzer havuz ve sosyal tesisler ile yönetim binalarını kapsayan Plan mevcut mu?       " },
+                            { id: "q-16-3", head: "Sahada/çalışma alanlarında uygun yerlerde plan mevcut mu?" },
+                            { id: "q-16-3", head: "Acil durumlarda çalışanların tahliyesi için kullanılacak yollar belirlenmiş mi?" },
+                            { id: "q-16-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="17"
+                    head="Sabit ve seyyar yangınla mücadale/söndürme tesisat ve cihazları kullanıma hazır mı?"
+                    items={
+                        [
+                            { id: "q-17-1", head: "Ek 17" },
+                            { id: "q-17-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Örnek formlar sunulmuş mu?" },
+                            { id: "q-17-3", head: "Uygulama gözlendi mi? (Yedek pompa ve gerekli ise pompanın yedek güç beslemesinin kullanıma hazır olduğunun gözlenmesinde fayda var.)" },
+                        ]} />
+                <QuestionBody
+                    id="18"
+                    head="Tesiste yer alan bölümler ve yapılar işaret levhaları ile markalanmış mı?"
+                    items={
+                        [
+                            { id: "q-18-1", head: "Ek 18 (Genel izlenim verebilecek fotoğraflar)" },
+                            { id: "q-18-2", head: "Uygulama gözlendi mi / Yapı ve alanlar maksadına uygun kullanılıyor mu?" },
+                        ]} />
+                <QuestionBody
+                    id="19"
+                    head="Tesis içi yollar belirlenmiş ve araç/çalışan kullanımı için açık durumda mı?"
+                    items={
+                        [
+                            { id: "q-19-1", head: "Ek 19" },
+                            { id: "q-19-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Örnek formlar sunulmuş mu?" },
+                            { id: "q-19-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="20"
+                    head="İdari ve sosyal tesisler, dinlenme yeri, yemekhane, lavabo ve tuvaletler mevcut mu?"
+                    items={
+                        [
+                            { id: "q-20-1", head: "Ek 20" },
+                            { id: "q-20-2", head: "Çalışan sayısı ile uyumlu tesis/alan belirlenmiş ve düzenlenmiş mi?" },
+                            { id: "q-20-3", head: "İlgili alanların genel hijyen, düzen ve olanakları yeterli mi? (Soyunma odası imkanları (temiz gündelik kıyafet ve iş kıyafeti için ayrı dolaplar, yeter sayıda duş/tuvalet, çamaşır yıkama imkanı vb.)" },
+                        ]} />
+                <QuestionBody
+                    id="21"
+                    head="İçme suyu imkanı mevcut mu?"
+                    items={
+                        [
+                            { id: "q-21-1", head: "Ek 21" },
+                            { id: "q-21-2", head: "Çalışan sayısı ile uyumlu tesis/alan belirlenmiş ve düzenlenmiş mi?" },
+                            { id: "q-21-3", head: "İlgili alanların genel hijyen, düzen ve olanakları yeterli mi? (Soyunma odası imkanları (temiz gündelik kıyafet ve iş kıyafeti için ayrı dolaplar, yeter sayıda duş/tuvalet, çamaşır yıkama imkanı vb.)" },
+                        ]} />
+                <QuestionBody
+                    id="22"
+                    head="Çalışanların ve araçların giriş-çıkış yaptığı alanlar, güvenliği sağlayıcı ve kayıtların tutulabileceği şekilde düzenlenmiş durumda mı?"
+                    items={
+                        [
+                            { id: "q-22-1", head: "Ek 22" },
+                            { id: "q-22-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Örnek formlar sunulmuş mu?" },
+                            { id: "q-22-3", head: "Ziyaretçiler için prosedürler geliştirilmiş mi? (Tesis/Acil durum bilgilendirme, KKD temini vb.)" },
+                            { id: "q-22-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="23"
+                    head="Revir/ilk yardım üniteleri ve teçhizatları mevcut mu?"
+                    items={
+                        [
+                            { id: "q-23-1", head: "Ek 23 " },
+                            { id: "q-23-2", head: "Belirlenmiş alanların genel hijyen, düzen ve olanakları yeterli mi?" },
+                            { id: "q-23-3", head: "Bulundurulması öngörülmüş tıbbi malzeme belirlenmiş ve kontrolü ile ilgili prosedür geliştirilmiş mi? Prosedürün uygulandığı gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="24"
+                    head="İşyeri sağlık ve güvenlik birimi (İSGB)/ Ortak sağlık ve güvenlik birimi (OSGB) ile gerekli sözleşmeler yapılmış mı?"
+                    items={
+                        [
+                            { id: "q-24-1", head: "Ek 24 (KATİB sistemi çıktıları)" },
+                            { id: "q-24-2", head: "Tesis/Firma bilgileri Ticaret Sicil Gazetesi ile uyumlu mu?" },
+                            { id: "q-24-3", head: "İlgili belgelerde faaliyet konusu ve tehlike sınıfı fiili durumla uyumlu mu?" },
+                        ]} />
+                <QuestionBody
+                    id="25"
+                    head="Her türlü sabit ve hareketli kaldırma vinçlerini kullanan operatörler ile çalıştırılan diğer operatör ve kalifiye elemanlar sertifikalı mı? (Yönetmelik Md 17 (7) a)"
+                    items={
+                        [
+                            { id: "q-25-1", head: "Ek 25 (Mevcut ise çalışanların öğrenim durumu/Mesleki Yeterlilik Belgesi/Sertifikalarını gösterir doküman. Personel listesi) " },
+                            { id: "q-25-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Örnek sertifikalar sunulmuş mu?" },
+                            { id: "q-25-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="26"
+                    head="TÜRKAK A tipi muayene kuruluşu olarak yetki alan kuruluşlara taşınabilir veya taşınamaz basınçlı ekipmanların, boru hatlarının kontrolleri yaptırılmış mı?"
+                    items={
+                        [
+                            { id: "q-8-1", head: "Ek 26 " },
+                            { id: "q-8-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Envanter takibi yapılabiliyor mu? Örnek formlar sunulmuş mu?" },
+                            { id: "q-8-3", head: "Muayene raporları sunuldu mu?" },
+                            { id: "q-8-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
+                <QuestionBody
+                    id="27"
+                    head="TÜRKAK A tipi muayene kuruluşu olarak yetki alan kuruluşlara ırgat, kaldırma ve iletme makinalarının kontrolleri yaptırılmış mı?"
+                    items={
+                        [
+                            { id: "q-8-1", head: "Ek 27" },
+                            { id: "q-8-2", head: "Yönetim sisteminde ilgili prosedürler geliştirilmiş mi? Envanter takibi yapılabiliyor mu? Örnek formlar sunulmuş mu?" },
+                            { id: "q-8-3", head: "Muayene raporları sunuldu mu?" },
+                            { id: "q-8-3", head: "Uygulama gözlendi mi?" },
+                        ]} />
             </section>
             <table className="full-width">
                 <tbody>
