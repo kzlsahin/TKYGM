4faed5ec1bcf988af04e34ebcd7ddfa8eb3a003e
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-11-27 16:16:10 +0300

    release - v1

--- src/TKYGM_server/ITDK_app/sw.v.0.3.js
+++ src/TKYGM_server/ITDK_app/sw.v.0.3.js
@@ -14,9 +14,9 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v0.14';
+const PRECACHE = 'precache-v0.15';
 const RUNTIME = 'runtime';
-const BASEURL = `https://kzlsahin.online/TKYGM/ITDK-v0`;
+const BASEURL = `https://kzlsahin.online/TKYGM/ITDK-v1`;
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
